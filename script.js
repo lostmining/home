9a5dde657961d3c5eae12a89a0cfe7a9113b0ecf
lostmining <lymona10k@gmail.com>
2025-11-20 15:07:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<script type='text/javascript'>
+type='text/javascript'>
     //<![CDATA[
     var uri = window.location.toString();
     if (uri.indexOf("%3D", "%3D") > 0) {
@@ -18,8 +18,8 @@
         window.history.replaceState({}, document.title, clean_uri);
     }
     //]]>
-    </script>
-    <script>
+    
+ 
         document.addEventListener("DOMContentLoaded", function () {
             const lazyImages = document.querySelectorAll("img[data-src]");
             const imageObserver = new IntersectionObserver((entries, observer) => {
@@ -34,8 +34,7 @@
             });
             lazyImages.forEach(image => imageObserver.observe(image));
         });
-    </script>
-    <script>
+   
         document.addEventListener('contextmenu', function(e) {
             e.preventDefault();
         });
@@ -45,14 +44,9 @@
         document.addEventListener('copy', function(e) {
             e.preventDefault();
         });
-    </script>
-<!-- Bootstrap 5 JS Bundle -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" xintegrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     
-    <!-- AOS Animation Library JS -->
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    
-    <script>
+
+  
         // Initialize AOS
         AOS.init({
             duration: 1000,
@@ -73,6 +67,5 @@
             popup.style.display = "none";
             iframe.src = ""; // Clear src to stop loading
         }
-    </script>
-<script>const allowedDomain = "www.cryptoapp.store";   const currentDomain = window.location.hostname;    if (currentDomain !== allowedDomain && currentDomain !== "www." + allowedDomain) {   window.location.href = "https://" + allowedDomain + window.location.pathname + window.location.search;  }
-</script>
+    const allowedDomain = "www.cryptoapp.store";   const currentDomain = window.location.hostname;    if (currentDomain !== allowedDomain && currentDomain !== "www." + allowedDomain) {   window.location.href = "https://" + allowedDomain + window.location.pathname + window.location.search;  }
+
